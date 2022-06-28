f7a264ea689ba5e4ad04dae8b3fd1280a21a6d99
Darryl Brooks <darryl.brooks@aa.com>
2022-06-28 14:18:26 -0500

    Initial Commit

---
+++ Thundurus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thundurus
+//
+//  Created by Darryl Brooks on 6/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thundurus/ThundurusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThundurusApp.swift
+//  Thundurus
+//
+//  Created by Darryl Brooks on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ThundurusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
