f2d5db40a0b10c85ff209a7a71aaca32c35539a9
Darryl Brooks <darryl.brooks@aa.com>
2022-06-28 16:27:08 -0500

    feat: init commit

---
+++ Thundurus/Core/Homepage/HomepageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomepageView.swift
+//  Thundurus
+//
+//  Created by Darryl Brooks on 6/28/22.
+//
+
+import SwiftUI
+
+struct HomepageView: View {
+    var body: some View {
+        Text("Thundurus Homepage")
+    }
+}
+
+struct HomepageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomepageView()
+    }
+}
