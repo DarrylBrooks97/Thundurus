a160c23ab361ac45137bbbb712219556e459814f
Darryl Brooks <darryl.brooks@aa.com>
2022-07-07 10:31:00 -0500

    feat: updating home to show diff views

--- Thundurus/ThundurusApp.swift
+++ Thundurus/ThundurusApp.swift
@@ -1,17 +1,18 @@
-//
-//  ThundurusApp.swift
-//  Thundurus
-//
-//  Created by Darryl Brooks on 6/28/22.
-//
 
 import SwiftUI
 
 @main
 struct ThundurusApp: App {
+    @State private var showWeather:Bool = false;
+    @State private var location:String = "";
+    
     var body: some Scene {
         WindowGroup {
-            HomepageView()
+            if(!showWeather){
+                HomepageView(showWeather: $showWeather, location: $location)
+            }else{
+                ContentView();
+            }
         }
     }
 }
